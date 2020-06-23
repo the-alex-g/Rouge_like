8fd52c989a61223b723efea137d25a5dcdf4f954
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-06-23 11:34:03 -0400

    Add Main scene

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,11 +1,16 @@
 extends KinematicBody2D
 
+enum State {IDLE, WALKING}
 onready var sprite:Sprite = $Sprite
 export var speed:int = 200
+var state = State.IDLE
 var screensize:Vector2 = Vector2.ZERO
 
 func _ready():
 	screensize = get_viewport_rect().size
+
+func _draw():
+	draw_circle(self.position, 10, Color.silver)
 
 func _process(delta):
 	var _velocity:Vector2 = Vector2.ZERO
@@ -17,5 +22,14 @@ func _process(delta):
 		_velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
 		_velocity.x -= 1
+	if _velocity.x != 0:
+		sprite.flip_v = false
+		sprite.flip_h = _velocity.x < 0
+		state = State.WALKING
+	elif _velocity.y != 0:
+		sprite.flip_v = _velocity.y > 0
+		state = State.WALKING
+	elif _velocity == Vector2.ZERO:
+		state = State.IDLE
 	_velocity = _velocity.normalized()*speed*delta
-	move_and_collide(_velocity)
+	var _error = move_and_collide(_velocity)
